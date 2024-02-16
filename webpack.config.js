ed0f9ad60042839a42bb7e882b769aa86a7abfa1
Gavin O Brien <142741214+Gaviobm1@users.noreply.github.com>
2024-02-16 12:16:30 +0100

    Make project template with dependencies ready

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "./src/bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(woff|woff2|otf|ttf|eot)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: "Document",
+      inject: "body",
+    }),
+  ],
+  watch: true,
+};
